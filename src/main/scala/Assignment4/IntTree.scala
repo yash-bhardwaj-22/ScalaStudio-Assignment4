6c967ba7739140b60349e29f8cac8e3c8bfbb40c
yash-bhardwaj-22 <yash.bhardwaj@knoldus.com>
2021-10-27 10:23:31 +0530

    new commit

--- src/main/scala/Assignment4/IntTree.scala
+++ src/main/scala/Assignment4/IntTree.scala
@@ -4,3 +4,26 @@ abstract class IntTree
 case object EmptyTree extends IntTree
 case class Node(elem: Int, left: IntTree, right: IntTree) extends IntTree
 
+object NewTree {
+  def contains(t: IntTree, v: Int): Boolean = t match {
+    case EmptyTree => false
+    case node: Node if node.elem == v => true
+    case node: Node => contains(node.left, v) || contains(node.right, v)
+  }
+
+  def insert(t: IntTree, v: Int): IntTree = t match {
+    case EmptyTree
+    => Node(v, EmptyTree, EmptyTree)
+    case node: Node if size(node.left) < size(node.right)
+    => Node(node.elem, insert(node.left, v), node.right)
+    case node: Node
+    => Node(node.elem, node.left, insert(node.right, v))
+  }
+
+  def size(intTree: IntTree): Int = {
+    intTree match {
+      case EmptyTree => 0
+      case node: Node => 1 + size(node.left) + size(node.right)
+    }
+  }
+}
--- src/main/scala/Assignment4/IntersectionAndUnion.scala
+++ src/main/scala/Assignment4/IntersectionAndUnion.scala
@@ -4,9 +4,9 @@ object IntersectionAndUnion {
   def Union(set1: Set[Int],set2:Set[Int] ):Set[Int]= set1 ++ set2
 
   def Intersection(set1: Set[Int], set2: Set[Int]): Set[Int] = {
-    val i:Set[Int]=Set()
+    val temp:Set[Int]=Set()
     if(set1.isEmpty || set2.isEmpty){
-      i
+      temp
     }
     val temp2=set1--set2
     set1--temp2
